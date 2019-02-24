bdeea8ab708128d6384e5d0ff4b698bdfdfd1a51
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-02-23 21:09:07 -0500

    added thumbnails and addEventListener to gallery

--- gallery.js
+++ gallery.js
@@ -1,11 +1,18 @@
 'use strict';
 
-function getFullImg(img){
-    const fullImg = document.getElementById('full-img');
-    fullImg.src = img.src;
-
-    const imgText = document.getElementById('img-text');
-    imgText.innerHTML = `<span>${img.alt}</span>`;
+function getFullImg(){
+    const imgUl = document.querySelector('.img-thumbnails');
+    imgUl.addEventListener('click', event => {
+        event.stopPropagation;
+        const clickedElem = event.target;
+        if (clickedElem.className === 'thumbnail'){
+            const fullImg = document.getElementById('full-img');
+            fullImg.src = `media/full-size-imgs/img${clickedElem.id}.jpg`;
+        
+            const imgText = document.getElementById('img-text');
+            imgText.innerHTML = `<span>${clickedElem.alt}</span>`;
+        }
+    })
 }
 
 function generateThumbnails(num){
@@ -13,11 +20,12 @@ function generateThumbnails(num){
     imgUl.innerHTML = '';
     for (let i = 1; i <= num; i++){
         const imgLi = document.createElement('li');
-        const imgLink = `<img class="thumbnail" src="media/full-size-imgs/img${i}.jpg" alt="caption text for img${i}.jpg" onclick="getFullImg(this)" />`;
+        const imgLink = `<img class="thumbnail" src="media/thumbnails/img${i}_tn.jpg" alt="caption text for img${i}.jpg" id="${i}" />`;
         imgLi.innerHTML = imgLink;
         imgUl.appendChild(imgLi);
     }
 
 }
 
-generateThumbnails(7);
+generateThumbnails(24);
+getFullImg();
