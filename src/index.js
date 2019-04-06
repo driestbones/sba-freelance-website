bc6655eaeb5d99b4b3bb38986b4e72bfcdf8d82a
chuckha <ha.chuck@gmail.com>
2019-04-06 08:54:15 -0400

    Update to use bulma
    Signed-off-by: chuckha <ha.chuck@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// necessary require for webpack to discover css
+require('./styles.scss');
+
+var burgers  = document.querySelectorAll(".burger");
+for (var i=0; i<burgers.length; i++) {
+    var b = burgers.item(i);
+    var dataTarget = b.dataset["target"];
+    var target = document.getElementById(dataTarget)
+
+    b.addEventListener("click", function() {
+        b.classList.toggle("is-active");
+        target.classList.toggle("is-active");
+    });
+}
